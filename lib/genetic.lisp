6f1417ef0f006eb53fff65af32ed1f9aa32e9f8b
Gordon Zheng <gordon@greenhouse.io>
2016-11-03 17:16:05 -0400

    Add more genetic algorithm boilerplate

--- lib/genetic.lisp
+++ lib/genetic.lisp
@@ -1,13 +1,50 @@
-(defun fitness (individual guesses responses))
-
 (defvar *guesses* nil)
 (defvar *responses* nil)
+
+(defparameter *population-size* 10)
+(defvar *population* nil)
 
 (defun first-guess (board colors)
   "Make a guess with as many colors as possible"
   (loop for i from 0 to (- board 1)
     for color_index = (mod i (length colors))
     collect (nth color_index colors)))
+
+(defun initial-population (board colors)
+  "Generate the initial population of size *population-size* using individuals with randomly picked colors."
+  ; TODO: ensure there are no duplicate individuals
+  (loop for i from 1 to *population-size*
+    collect (insert-colors board colors)))
+
+(defun fitness (individual guesses responses)
+  "Determines fitness of individual based on how consistent it is with past guesses and responses")
+
+(defun random-selection (population guesses responses)
+  "Chooses a individual from the population with a bias for fitness")
+
+(defun reproduce (parent-a parent-b)
+  "Returns a single individual offspring from two individuals")
+
+(defun mutate (individual)
+  "Mutate the individual")
+
+(defun mutate-with-chance (individual chance)
+  "Choose to mutate the individual based on some probability")
+
+(defun genetic-algorithm (guesses responses)
+  "Breeds a new generation and returns its most fit individual"
+  (let
+    ((new-population
+      (loop for _ in *population*
+        for parent-a = (random-selection *population* guesses responses)
+        for parent-b = (random-selection *population* guesses responses)
+        for child = (reproduce parent-a parent-b)
+        for possibly-mutated-child = (mutate-with-chance child 0.05)
+        collect possibly-mutated-child)))
+    (setf *population* new-population)
+
+    ; TODO: sort new population by fitness and pick most fit
+    (first new-population)))
 
 ; Genetic team.  Interfaces with the game
 (defun Genetic (board colors SCSA last-response)
@@ -17,8 +54,15 @@
   (when (null last-response)
     (setf *guesses* nil)
     (setf *responses* nil)
+    ; TODO: ensure population does not include first guess
+    (setf *population* (initial-population board colors))
     (push (first-guess board colors) *guesses*)
     (return-from Genetic (first-guess board colors)))
 
   ; record last response
-  (push last-response *responses*))
+  (push last-response *responses*)
+
+  (let ((guess (genetic-algorithm *guesses* *responses*)))
+    (push guess *guesses*)
+    guess))
+  
--- test/genetic.lisp
+++ test/genetic.lisp
@@ -1,7 +1,13 @@
 (load "lib/genetic.lisp")
 
-(plan 1)
+(plan 2)
 
 (is '(A B C A B) (first-guess 5 '(a b c)))
+
+; ensure initial population is of size *population-size*
+(is *population-size* (length (initial-population 4 '(A B C D E F))))
+
+; TODO: ensure each individual in initial population contains only valid colors
+; TODO: ensure each individual in initial population is of the correct length
 
 (finalize)
