aa1ace7af4f52e8ee29278c7fe0c1102d82cfabc
Gordon Zheng <gordon@greenhouse.io>
2016-11-05 22:49:52 -0400

    Add tests for random selection and relative fitness

--- test/genetic.lisp
+++ test/genetic.lisp
@@ -32,15 +32,41 @@
         '((A A A A) (B B B B) (G G G G))
         '((1 1) (1 1) (1 1)))))
 
-  (is
-    '(
-      ((A B C D) 0.5)
-      ((F F F F) 0.16666667)
-      ((G D F B) 0.33333334))
-    (population-by-relative-fitness
-      '((A B C D) (F F F F) (G D F B))
-      *test-colors*
-      '((A A F D) (B B B B) (F C C C) (A G F B))
-      '((2 0) (1 0) (1 0) (2 1)))
-    "population-by-relative-fitness returns expected result"))
-    
+  (let
+    ; set some values to re-use in our tests:
+    (
+      (population '((A B C D) (F F F F) (G D F B)))
+      (guesses '((A A F D) (B B B B) (F C C C) (A G F B)))
+      (responses '((2 0) (1 0) (1 0) (2 1))))
+
+    (is
+      '(
+        ((A B C D) 0.5)
+        ((F F F F) 0.16666667)
+        ((G D F B) 0.33333334))
+      (population-by-relative-fitness
+        population
+        *test-colors*
+        guesses
+        responses)
+      "population-by-relative-fitness maps individuals to their fitness relative to the rest of population")
+
+    (ok
+      (let*
+        (
+          (selections
+            (loop
+              for i from 1 to 1000
+              collect (random-selection population *test-colors* guesses responses)))
+          (count-abcd
+            (count '(A B C D) selections :test #'equal))
+          (count-ffff
+            (count '(F F F F) selections :test #'equal)))
+        (and
+          (and
+            (< count-abcd 550)
+            (> count-abcd 450))
+          (and
+            (< count-ffff 200)
+            (> count-ffff 100))))
+      "random-selection selects individuals with probability based on their fitness")))
