d6c7aeba980ef3f6a53fffc0261c764ede798ab2
NSharker <nishad.sharker@gmail.com>
2016-11-27 14:05:01 -0500

    Converted functions to return True, added prefer fewer matching, added test-SCSA
    -Converted all SCSA checker functions to return true if they match the
    SCSA pattern
    -implemented a possible way to detect prefer fewer, mystery2 and myster5
    by using ratios of appearance of colors and difference from our observed
    answers
    -added a test-SCSA function that lets us input a guess to see if the
    matchers correctly categorize the guess
    -added 2-color-alt and random-scsa bucket
    -added 2-color-alt checker function
    -fixed mystery3 and AB checker's equals, previously tried to check equal
    between 3 items.
    -made defparameters for colors-in-guess, sorted-count for mystery3 and
    result for ab-checker
    -added possible code for detecting game number

--- lib/NishadSCSA.lisp
+++ lib/NishadSCSA.lisp
@@ -9,6 +9,7 @@
 
 	 
 
+;;TODO Convert the checkers into predicates so we can use them to determine our guess follows the SCSA
 
 
 
@@ -28,6 +29,8 @@
 
 (defparameter *AB-SCSA* 0)
 
+(defparameter *2-color-alt-SCSA* 0)
+
 (defparameter *at-most-once*  0)
 
 (defparameter *first-last* 0)
@@ -36,6 +39,7 @@
 
 (defparameter *preference-fewer* 0)
 
+(defparameter *random-SCSA* 0)
 
 ;;Patterns
 ;;;
@@ -87,8 +91,8 @@
 (defun mystery-1 (board)
   (setf *mystery-1-pattern* (loop for i below board collect (mod i 3))))
 
-(defun mystery-1-checker (guess)
-  (if (equal *mystery-1-pattern* guess) (setf *mysytery-1* (+ 1 *mystery-1*))))
+(defun mystery-1-checker-p (guess)
+  (if (equal *mystery-1-pattern* guess) T ))
   
   
 
@@ -111,9 +115,13 @@
 ;;; (mod board 3) == 1 only 1 element should have 1 greater
 ;;; (mod board 3) == 2 only 2 elements should have 1 greater
 
+;;TODO need to maybe do a defparameter for colors-in-guess and sorted-count
+(defparameter colors-in-guess nil)
+(defparameter sorted-count nil)
 
-;;ERROR here can't use Equal to check all 3, Equal can only check 2 at a time.
-(defun mystery-3-checker (guess)
+
+
+(defun mystery-3-checker-p (guess)
   (if (equal (length (remove-duplicates guess)) 3)
       (progn (setq colors-in-guess (remove-duplicates guess))
 	     (setq sorted-count (sort (list
@@ -122,18 +130,19 @@
 				       (count (third colors-in-guess) guess)) #'<))
 	     (cond
 	       ((and (equal (mod (length guess) 3) 0)
-		     (equal (first sorted-count)
-			    (second sorted-count)
-			    (third sorted-count)))
-		(setf *mystery-3* (+ 1 *mystery-3*)))
+		     (and (equal (first sorted-count)
+				 (second sorted-count))
+			  (equal (first sorted-count)
+				 (third sorted-count))))
+		T)
 	       ((and (equal (mod (length guess) 3) 1)
 		     (equal (first sorted-count) (second sorted-count))
-		     (equal (third sorted-count) (- (first sorted-count) 1)))
-		(setf *mystery-3* (+ 1 *mystery-3*)))
+		     (equal (third sorted-count) (+ (first sorted-count) 1)))
+		T)
 	       ((and (equal (mod (length guess) 3) 2)
 		     (equal (second sorted-count) (third sorted-count))
 		     (equal (third sorted-count) (+ (first sorted-count) 1)))
-		(setf *mystery-3* (+ 1 *mystery-3*)))))))
+		T)))))
 
 
 
@@ -169,8 +178,8 @@
 (defun mystery-4 (board)
   (setf *mystery-4-pattern* (loop for i below board collect (mod i 2))))
 
-(defun mystery-4-checker (guess)
-  (if (equal *mystery-4-pattern* guess) (setf *mystery-4* (+ 1 *mystery-4*))))
+(defun mystery-4-checker-p (guess)
+  (if (equal *mystery-4-pattern* guess) T))
 
 
        
@@ -188,44 +197,49 @@
 
 ;;2-color list
 ;; remove duplicates from guess, if is length 2 then this is true
-(defun 2-color-checker (guess)
-  (if (equal (length (remove-duplicates guess)) 2)(setf *2-color-SCSA* (+ 1 *2-color-SCSA*))))
+(defun 2-color-checker-p (guess)
+  (if (equal (length (remove-duplicates guess)) 2) T))
 
 
 
 
 ;;list of only AB
 ;; remove duplicates from guess if the list is length 2 and only has A and B this is true
-(defun AB-checker (guess)
-  ((setq result (remove-duplicates guess))
-   (if (and (equal (length result) 2) (member A result) (member B result)) (setf *AB-SCSA* (+ 1 *AB-SCSA*)))))
+(defparameter result nil)
+
+(defun AB-checker-p (guess)
+  (progn(setq result (remove-duplicates guess))
+	(if (and (equal (length result) 2) (member 'A result) (member 'B result)) T)))
      
 
 
 ;;list of alternate 2 colors
 ;;Use mystery-4 code
 
+(defun 2-color-alt-checker-p (guess)
+  (if (equal *mystery-4-pattern* guess) T))
+
 
 
 ;;a list in which colors appear at most once
 ;;remove duplicates in a guess and see if length changes, if not then true
-(defun at-most-once-checker (guess)
-  (if (equal (length guess) (length (remove-duplicates guess))) (setf *at-most-once* (+ 1 *at-most-once*))))
+(defun at-most-once-checker-p (guess)
+  (if (equal (length guess) (length (remove-duplicates guess))) T))
 
 
 
 
 ;;first and last are same
 ;;just check first and last of guess to be equal
-(defun first-last-checker (guess)
-  (if (equal (first guess) (last guess))(setf *first-last* (+ 1 *first-last*))))
+(defun first-last-checker-p (guess)
+  (if (equal (first guess) (last guess)) T))
 
 
 
 ;;Fewer colors (2 or 3)
 ;;remove duplicates from guess and check length if lenght is <= 3 then true
-(defun less-than-three-checker (guess)
-  (if (<= (length (remove-duplicates guess)) 3) (setf *less-than-three* (+ 1 *less-than-three* 0))))
+(defun less-than-three-checker-p (guess)
+  (if (<= (length (remove-duplicates guess)) 3) T))
 
 
 
@@ -236,29 +250,9 @@
 ;; 8% chance to have 4 colors
 ;; 3% chance to have 5 colors
 ;; 1% chance to have 6 colors
-(defun preference-fewer-checker (guess)
-  ( if (<= (length (remove-duplicates guess)) 6) (setf *preference-fewer* (+ 1 *preference-fewer*))))
-
-
-
-;;Flawed right now
-;;Need to determine whether this is a new tournament or old.
-;;last-response is a guess or a 3 tuple?
-(defun SCSA-guesser (board colors SCSA last-response)
-  (progn (when (null last-response)
-	   (reset-SCSA-stats)
-	   (convert-guess last-response)
-	   (mystery-1 board)
-	   (mystery-4 board))
-	 (mystery-1-checker *converted-to-generic*)
-	 (mystery-3-checker last-response)
-	 (mystery-4-checker last-response)
-	 (2-color-checker last-response)
-	 (AB-Checker last-response)
-	 (at-most-once-checker last-response)
-	 (first-last-checker last-response)
-	 (less-than-three-checker last-response)
-	 (preference-fewer-checker last-response)))
+;;(defun preference-fewer-checker-p (guess)
+;;  ( if (<= (length (remove-duplicates guess)) 6) T))
+  
 
 
 ;;Resets all our stats related to scsa
@@ -270,6 +264,7 @@
 	 (setf *mystery-5* 0)
 	 (setf *2-color-SCSA* 0)
 	 (setf *AB-SCSA* 0)
+	 (setf *2-color-alt-SCSA* 0)
 	 (setf *at-most-once* 0)
 	 (setf *first-last* 0)
 	 (setf *less-than-three* 0)
@@ -277,3 +272,156 @@
 	 (setf *converted-to-generic* nil)
 	 (setf *mystery-1-pattern* nil)
 	 (setf *mystery-4-pattern* nil)))
+
+
+
+
+;;To see if the functions work properly
+(defun SCSA-Tester (guess board)
+  (progn (mystery-1 board)
+	 (mystery-4 board)
+	 (convert-guess guess)
+	 (format t "~%Mystery-1-checker-p: ~a" (mystery-1-checker-p *converted-to-generic*))
+	 (format t "~%Mystery-3-checker-p: ~a" (mystery-3-checker-p guess))
+	 (format t "~%Mystery-4-checker-p: ~a" (mystery-4-checker-p *converted-to-generic*))
+	 (format t "~%2-color-checker-p: ~a" (2-color-checker-p guess))
+	 (format t "~%AB-checker-p: ~a" (AB-checker-p guess))
+	 (format t "~%2-color-alt-checker-p: ~a" (2-color-alt-checker-p guess))
+	 (format t "~%at-most-once-checker-p: ~a" (at-most-once-checker-p guess))
+	 (format t "~%first-last-checker-p: ~a" (first-last-checker-p guess))
+	 (format t "~%less-then-three-checker-p: ~a" (less-than-three-checker-p guess))))
+
+
+;;SCSA-Matcher assumes you have mystery-1 and mystery-4 patterns set
+(defun SCSA-Matcher (guess)
+  (progn (convert-guess guess)
+	 (if (mystery-1-checker-p *converted-to-generic*) (setf *mystery-1* (+ *mystery-1* 1)))
+	 (if (mystery-3-checker-p guess) (setf *mystery-3* (+ *mystery-3* 1)))
+	 (if (mystery-4-checker-p *converted-to-generic*) (setf *mystery-4* (+ *mystery-4* 1)))
+	 (if (2-color-checker-p guess) (setf *2-color-SCSA* (+ *2-color-SCSA* 1)))
+	 (if (AB-checker-p guess) (setf *AB-SCSA* (+ *AB-SCSA* 1)))
+	 (if (2-color-alt-checker-p guess) (setf *2-color-alt-SCSA* (+ *2-color-alt-SCSA* 1)))
+	 (if (at-most-once-checker-p guess) (setf *at-most-once* (+ *at-most-once* 1)))
+	 (if (first-last-checker-p guess) (setf *first-last* (+ *first-last* 1)))
+	 (if (less-than-three-checker-p guess) (setf *less-than-three* (+ *less-than-three* 1)))
+	 (setf *random-SCSA* (+ *random-SCSA* .75))))
+
+
+(defun display-buckets ()
+  (progn (format t "~% *mystery-1*: ~a" *mystery-1*)
+	 (format t "~% *mystery-2*: ~a" *mystery-2*)
+	 (format t "~% *mystery-3*: ~a" *mystery-3*)
+	 (format t "~% *mystery-4*: ~a" *mystery-4*)
+	 (format t "~% *mystery-5*: ~a" *mystery-5*)
+	 (format t "~% *2-color-SCSA*: ~a" *2-color-SCSA*)
+	 (format t "~% *AB-SCSA*: ~a" *AB-SCSA*)
+	 (format t "~% *2-color-alt-SCSA*: ~a" *2-color-alt-SCSA*)
+	 (format t "~% *at-most-once*: ~a" *at-most-once*)
+	 (format t "~% *first-last*: ~a" *first-last*)
+	 (format t "~% *less-than-three*: ~a" *less-than-three*)
+	 (format t "~% *preference-fewer*: ~a" *preference-fewer*)
+	 (format t "~% *random-SCSA*: ~a" *random-SCSA*)))
+	 
+
+
+
+
+
+
+
+
+;;Make a list of n colors with 0 in each position
+;;call color-feq it increments the list with the number of colors in it.
+
+(defparameter *solution-color* nil)
+
+(defun set-solution-color (colors)
+  (setf *solution-color* (make-list colors :initial-element 0)))
+
+
+(defun color-feq (guess)
+  (setf (nth (- (length (remove-duplicates guess)) 1) *solution-color*)
+	(+ (nth (- (length (remove-duplicates guess)) 1) *solution-color*) 1)))
+
+(defparameter *freq-divider* 1)
+
+(defun set-freq-divider ()
+  (setf *freq-divider* (apply #'+ *solution-color*)))
+
+(defparameter *solution-color-freq* nil)
+
+(defun make-color-freq-list ()
+  (setf *solution-color-freq*
+	(loop
+	   for x in *solution-color*
+	   collect (float (/ x freq-divider)))))
+
+
+(defparameter *pref-fewer-ratio* '(0.5 0.25 0.13 0.08 0.03 0.01))
+
+(defparameter *mystery-2-ratio* '(0.45 0.39 0.04 0.08 0.04))
+
+(defparameter *mystery-5-ratio* '(0.0 0.06 0.45 0.49 0.0))
+
+
+(defparameter *pref-fewer-diff* 0.0)
+
+(defparameter *mystery-2-diff* 0.0)
+
+(defparameter *mystery-5-diff* 0.0)
+
+
+(defun find-diff-pref-fewer ()
+  (setf *pref-fewer-diff*
+	(loop
+	   for x in *solution-color-freq*
+	   for y in *pref-fewer-ratio*
+	   sum (abs (- x y)))))
+
+(defun find-diff-mystery-2 ()
+  (setf *mystery-2-diff*
+	(loop
+	   for x in *solution-color-freq*
+	   for y in *mystery-2-ratio*
+	   sum (abs (- x y)))))
+
+(defun find-diff-mystery-5 ()
+  (setf *mystery-5-diff*
+	(loop
+	   for x in *solution-color-freq*
+	   for y in *mystery-5-ratio*
+	   sum (abs (- x y)))))
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;Game Number
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defparameter *game-number* 0)
+
+
+
+;;This should be called whenever we check previous guess = nil so we know we're in a new game
+(defun game-number-incrementer ()
+  (progn (when (>= *game-number* 100)
+	   (setf *game-number* 0))
+	 (setf *game-number* (+ *game-number* 1))))
+
+;;call reset function when games = 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
